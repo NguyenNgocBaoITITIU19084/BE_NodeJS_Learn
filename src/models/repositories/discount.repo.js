b1a4d4e30725acd2e5b8f82047b4ec05764a3b7d
NguyenNgocBaoITITIU19084 <ititiu19084@student.hcmiu.edu.vn>
2024-08-03 00:03:32 +0700

    discount features: create, getall

--- src/models/repositories/discount.repo.js
+++ src/models/repositories/discount.repo.js
@@ -1,1 +1,101 @@
 "use strict";
+
+const {
+  convertToObjectIdMongoose,
+  unGetSelectData,
+  getSelectData,
+} = require("../../utils/index");
+const discountModel = require("../../models/discount.model");
+
+const findByShopIdAndCode = async ({ code, shopId }) => {
+  return await discountModel
+    .findOne({
+      discount_code: code,
+      discount_shopId: convertToObjectIdMongoose(shopId),
+    })
+    .lean();
+};
+
+const createNewDiscount = async ({ payload }) => {
+  const {
+    name,
+    description,
+    type,
+    value,
+    code,
+    start_date,
+    end_date,
+    max_uses,
+    max_per_users,
+    min_oder_value,
+    shopId,
+    is_active,
+    applies_to,
+    product_ids,
+  } = payload;
+  return await discountModel.create({
+    discount_name: name,
+    discount_description: description,
+    discount_type: type,
+    discount_value: value, // 10.000 VND or 20%
+    discount_code: code, // discount Code
+    discount_start_date: new Date(start_date),
+    discount_end_date: new Date(end_date),
+    discount_max_uses: max_uses, // number of available discount
+    discount_max_per_users: max_per_users, // used times for each person
+    discount_min_order_value: min_oder_value || 0,
+    discount_shopId: shopId,
+    discount_is_active: is_active,
+    discount_applies_to: applies_to,
+    discount_product_ids: applies_to === "all" ? [] : product_ids,
+  });
+};
+
+const findAllDiscountCodeUnSelect = async ({
+  page = 1,
+  limit = 50,
+  sort = "ctime",
+  filter,
+  unSelect,
+  model,
+}) => {
+  const skip = (page - 1) * limit;
+  const sortBy = sort === "ctime" ? { _id: -1 } : { _id: 1 };
+  const doccuments = await model
+    .find(filter)
+    .sort(sortBy)
+    .skip(skip)
+    .limit(limit)
+    .select(unGetSelectData(unSelect))
+    .lean();
+
+  return doccuments;
+};
+
+const findAllDiscountCodeSelect = async ({
+  page = 1,
+  limit = 50,
+  sort = "ctime",
+  filter,
+  unSelect,
+  model,
+}) => {
+  const skip = (page - 1) * limit;
+  const sortBy = sort === "ctime" ? { _id: -1 } : { _id: 1 };
+  const doccuments = await model
+    .find(filter)
+    .sort(sortBy)
+    .skip(skip)
+    .limit(limit)
+    .select(getSelectData(unSelect))
+    .lean();
+
+  return doccuments;
+};
+
+module.exports = {
+  findByShopIdAndCode,
+  createNewDiscount,
+  findAllDiscountCodeUnSelect,
+  findAllDiscountCodeSelect,
+};
--- src/services/discount.service.js
+++ src/services/discount.service.js
@@ -1,27 +1,18 @@
 "use strict";
 
-const { BadRequestError } = require("../cores/error.response");
+const { BadRequestError, NotFoundError } = require("../cores/error.response");
+const { findAllProducts } = require("../models/repositories/product.repo");
+const {
+  findByShopIdAndCode,
+  createNewDiscount,
+  findAllDiscountCodeUnSelect,
+} = require("../models/repositories/discount.repo");
+const { convertToObjectIdMongoose } = require("../utils");
 const discountModel = require("../models/discount.model");
-const { convertToObjectIdMongoose } = require("../utils/index");
 
 class DiscountService {
   static async createDiscount(payload) {
-    const {
-      name,
-      description,
-      type,
-      value,
-      code,
-      start_date,
-      end_date,
-      max_uses,
-      max_per_users,
-      min_oder_value,
-      shopId,
-      is_active,
-      applies_to,
-      product_ids,
-    } = payload;
+    const { code, start_date, end_date, shopId } = payload;
 
     if (new Date() < new Date(start_date) || new Date() > new Date(end_date)) {
       throw new BadRequestError("Discount code is expired!");
@@ -30,34 +21,78 @@ class DiscountService {
     if (new Date(start_date) >= new Date(end_date))
       throw new BadRequestError("Start date must be less than end date");
 
-    const foundDiscount = await discountModel
-      .findOne({
-        discount_code: code,
-        discount_shopId: convertToObjectIdMongoose(shopId),
-      })
-      .lean();
+    const foundDiscount = await findByShopIdAndCode({ code, shopId });
 
     if (foundDiscount && foundDiscount.discount_is_active)
       throw new BadRequestError("Discount is existed!");
     // create index for discount code
 
-    const newDiscount = await discountModel.create({
-      discount_name: name,
-      discount_description: description,
-      discount_type: type,
-      discount_value: value, // 10.000 VND or 20%
-      discount_code: code, // discount Code
-      discount_start_date: new Date(start_date),
-      discount_end_date: new Date(end_date),
-      discount_max_uses: max_uses, // number of available discount
-      discount_max_per_users: max_per_users, // used times for each person
-      discount_min_order_value: min_oder_value || 0,
-      discount_shopId: shopId,
-      discount_is_active: is_active,
-      discount_applies_to: applies_to,
-      discount_product_ids: applies_to === "all" ? [] : product_ids,
-    });
+    const newDiscount = await createNewDiscount(payload);
     return newDiscount;
+  }
+
+  static async updateDiscount() {
+    // ...
+  }
+
+  // get all product with discount code by user
+  static async getAllDiscountCodesWithProduct({
+    code,
+    shopId,
+    userId,
+    limit,
+    page,
+  }) {
+    const foundDiscount = await findByShopIdAndCode({ code, shopId });
+
+    if (!foundDiscount && !foundDiscount.discount_is_active)
+      throw new NotFoundError("Discount is existed!");
+
+    const { discount_applies_to, discount_product_ids } = foundDiscount;
+    let products;
+    if (discount_applies_to === "all") {
+      products = await findAllProducts({
+        filter: {
+          product_shop: convertToObjectIdMongoose(shopId),
+          isPublic: true,
+        },
+        page: +page,
+        limit: +limit,
+        sort: "ctime",
+        select: ["product_name"],
+      });
+    }
+
+    if (discount_applies_to === "specific") {
+      products = await findAllProducts({
+        filter: {
+          _id: { $in: discount_product_ids },
+          isPublic: true,
+        },
+        page: +page,
+        limit: +limit,
+        sort: "ctime",
+        select: ["product_name"],
+      });
+    }
+
+    return products;
+  }
+
+  // get all product with discount code by shop
+  static async getAllDiscountCodeByShop({ limit, page, shopId }) {
+    const discount = await findAllDiscountCodeUnSelect({
+      page: +page,
+      limit: +limit,
+      model: discountModel,
+      filter: {
+        discount_shopId: convertToObjectIdMongoose(shopId),
+        discount_is_active: true,
+      },
+      unSelect: ["__v", "discount_shopId"],
+    });
+
+    return discount;
   }
 }
 
